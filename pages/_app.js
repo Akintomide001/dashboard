05334035c2960c675def84c4d1a5c109b8c77ed8
tommy <akintomide001@gmail.com>
2023-06-25 22:14:28 +0100

    dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '@/styles/globals.css'
+ import '@/styles/globals.css'
+import "@/styles/home.css"
+import "@/styles/header.css"
+import styles from "../styles/sidebar.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
