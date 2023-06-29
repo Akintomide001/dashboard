ec7ae26699ddda249434bafe4562fa39343773b3
tommy <akintomide001@gmail.com>
2023-06-29 10:05:33 +0100

    dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "@/styles/home.css"
 import "@/styles/header.css"
 import styles from "../styles/sidebar.css"
 import "@/styles/mobile.css"
+import "@/styles/responsiveness.css"
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
