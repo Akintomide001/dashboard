147c92a945cc2ae0b5f27611006d4d339ae8b3fc
tommy <akintomide001@gmail.com>
2023-07-06 20:09:12 +0100

    dashboard

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -10,17 +10,33 @@ import adminPlugins from '@iconify/icons-dashicons/admin-plugins';
 import adminUsers from '@iconify/icons-dashicons/admin-users';
 import adminTools from '@iconify/icons-dashicons/admin-tools';
 import adminSettings from '@iconify/icons-dashicons/admin-settings';
+import { useEffect, useRef, useState } from 'react'
 
 export default function Sidebar() {
+    const hideMenu = useRef()
+    const [currentlyShowing, setCurrentlyShowing] = useState(false)
+
+    useEffect(() => {
+        if (!currentlyShowing) {
+            hideMenu.current.style.display = "none"
+        } else {
+            hideMenu.current.style.display = "block"
+        }
+    }, [currentlyShowing])
     return (
         <>
             <div className="sidebar-1">
                 <div className="sidebar-2">
                     <div className="sidebar-2a">
-                        <li className="sidebar-menu">
+                        <li className="sidebar-menu" onClick={()=>setCurrentlyShowing(!currentlyShowing)}>
                             <a href="#"><Icon icon={dashboardIcon} className="iconify-1" /></a>
                             <a href="#" className="side-bar"><Icon icon={dashboardIcon} className="iconify" />Dashboard</a>
+
                         </li>
+                        <div className="sidebar-home" ref={hideMenu} style={{display: currentlyShowing ? "block" : "none"}}>
+                            <li className="home-1"><a href="">Home</a></li>
+                            <li className="update-1"><a href="">Update</a></li>
+                        </div>
                         <li className="sidebar-menu">
                             <a href=""><Icon icon={adminPost} className="iconify-1" /></a>
                             <a href="" className="side-bar"><Icon icon={adminPost} className="iconify" />Post</a>
@@ -31,7 +47,7 @@ export default function Sidebar() {
                         </li>
                         <li className="sidebar-menu">
                             <a href=""><Icon icon={adminPage} className="iconify-1" /></a>
-                            <a href=""className="side-bar"><Icon icon={adminPage} className="iconify" />Pages</a>
+                            <a href="" className="side-bar"><Icon icon={adminPage} className="iconify" />Pages</a>
                         </li>
                         <li className="sidebar-menu">
                             <a href=""><Icon icon={adminComments} className="iconify-1" /></a>
